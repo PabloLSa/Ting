5daf4209b0490186f8627f2610114b578c1595ca
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-07-31 19:12:27 -0300

    :sparkles:  PriorityQueue
    Implementei testes para a classe PriorityQueue capaz de armazenar arquivos pequenos de forma prioritária

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,28 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    pq = PriorityQueue()
+
+    task_1 = {"nome_do_arquivo": "arquivo1.txt", "qtd_linhas": 10}
+    task_2 = {"nome_do_arquivo": "arquivo2.txt", "qtd_linhas": 3}
+    task_3 = {"nome_do_arquivo": "arquivo3.txt", "qtd_linhas": 7}
+    task_4 = {"nome_do_arquivo": "arquivo4.txt", "qtd_linhas": 5}
+
+    pq.enqueue(task_1)
+    pq.enqueue(task_2)
+    pq.enqueue(task_3)
+    pq.enqueue(task_4)
+
+    assert len(pq) == 4
+
+    assert pq.search(1) == task_1
+
+    assert pq.dequeue() == task_2
+    assert pq.dequeue() == task_1
+    assert pq.dequeue() == task_3
+    assert len(pq) == 1
+    assert pq.dequeue() == task_4
+    assert len(pq) == 0
+

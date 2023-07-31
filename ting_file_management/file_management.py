ecc168fe0365195da0bac334364d9ef7881e0538
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-07-31 16:52:00 -0300

    :sparkles:  txt_importer
     Implementei uma função txt_importer dentro do módulo file_management capaz de importar notícias a partir de um arquivo TXT, utilizando "\n" como separador.~

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+def txt_importer(file_path):
+    mensagem_erro_formato_invalido = "Formato inválido"
+    mensagem_erro_arquivo_nao_encontrado = "Arquivo {file_path} não encontrado"
+
+    if not file_path.endswith(".txt"):
+        sys.stderr.write(mensagem_erro_formato_invalido + "\n")
+        return []
+
+    try:
+        with open(file_path, "r") as file:
+            result = file.read().splitlines()
+            return result
+    except FileNotFoundError:
+        sys.stderr.write(mensagem_erro_arquivo_nao_encontrado.format(file_path=file_path) + "\n")
+        return []
+
